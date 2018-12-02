8081f2f4a6f8faf1e10a710cc3be63d327ddc2c1
zhangtai <zhangtai@ieforever.com>
2018-12-02 13:50:11 +0800

    springboot整合freemarker

---
+++ src/main/java/com/zhangtai/demo/index/IndexController.java
@@ -0,0 +1,16 @@
+package com.zhangtai.demo.index;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+	@RequestMapping("/")
+	public String showIndex(Model model){
+		model.addAttribute("slogan", "智慧建筑方案提供商");
+		return "index";
+	}
+
+}
