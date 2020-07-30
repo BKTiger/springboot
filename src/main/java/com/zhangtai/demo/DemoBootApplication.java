f24a46a46b2c4fb000fbe858dd42fad331540ef8
zhangtai <bk_tiger@aliyun.com>
2020-07-30 15:51:30 +0800

    elk

--- src/main/java/com/zhangtai/demo/DemoBootApplication.java
+++ src/main/java/com/zhangtai/demo/DemoBootApplication.java
@@ -1,12 +1,23 @@
 package com.zhangtai.demo;
 
+import com.leimingtech.framework.elk.log.monitor.MonitorLogger;
+import com.leimingtech.framework.elk.log.monitor.MonitorLoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"com.leimingtech.*","com.zhangtai.demo.*"})
 public class DemoBootApplication {
 
+	// 声明监控日志常量
+	private static final MonitorLogger MONITOR_LOGGER = MonitorLoggerFactory.getMonitorLogger(DemoBootApplication.class);
+
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoBootApplication.class, args);
+		// 输出监控日志
+		MONITOR_LOGGER.info("200100", "监控日志应用启动成功！");
 	}
 }
--- src/main/java/com/zhangtai/demo/index/IndexController.java
+++ src/main/java/com/zhangtai/demo/index/IndexController.java
@@ -1,5 +1,6 @@
 package com.zhangtai.demo.index;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
+
 	@RequestMapping("/")
 	public String showIndex(Model model){
 		model.addAttribute("slogan", "智慧建筑方案提供商");
